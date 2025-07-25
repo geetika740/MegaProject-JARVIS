2efc1a29cfda930db0ce4066efe7ec42816c5844
Geetika kakkar <kakkargeetika200@gmail.com>
2025-07-25 15:31:32 +0530

    Initial commit of JARVIS virtual assistant project

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "Softly": "https://youtu.be/cWMxCE2HTag?si=-redeZfchttaBDoB",
+    "Sapphire": "https://youtu.be/JgDNFQ2RaLQ?si=cy3GFjbdJVlUy1IB",
+    "Afreen": "https://youtu.be/kw4tT7SCmaY?si=Soh-xxYvOAdcsDm-"
+
+}
